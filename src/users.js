5b8efbc3cb0abb375f542b8d7ba5dffb1ed393bf
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-24 14:44:40 +0530

    add user table

--- src/users.js
+++ src/users.js
@@ -1,10 +1,36 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm.js";
+import UsersTable from "./UsersTable.js";
 
+const users =[
+    {
+        id: 1,
+        name: 'John Doe'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe'
+    },
+    {
+        id: 3,
+        name: 'Johsss'
+    },
+    {
+        id: 4,
+        name: 'Smith'
+    }
+];
 const Users = () => {
     return (
-        <div>
+        <Box
+            sx={{
+                width:'calc(100% - 100px)',
+                margin: 'auto',
+                marginTop: '100px',
+            }}>
             <UserForm />
-        </div>
+            <UsersTable props rows={users}/>
+        </Box>
     );
 }
 
